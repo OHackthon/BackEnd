9f1e20e65ce44475d3e977a454e0583b03abca98
joaovnpt <joaovitorguiotti8@gmail.com>
2025-11-21 14:12:49 -0300

    FIX: arrumar tabela coleção

--- src/core/backend/models/colecao.py
+++ src/core/backend/models/colecao.py
@@ -4,7 +4,7 @@ from .colecionador import Colecionador
 class Colecao(models.Model):
     nome_colecao = models.CharField(max_length=100)
     colecionador = models.ForeignKey(Colecionador, on_delete=models.PROTECT)
-    descricao = models.CharField(max_length=255 null=True, blank=True)
+    descricao = models.CharField(max_length=255, null=True, blank=True)
     
     # 'auto_now_add' preenche a data automaticamente na criação
     data_registro_sistema = models.DateTimeField(auto_now_add=True)
