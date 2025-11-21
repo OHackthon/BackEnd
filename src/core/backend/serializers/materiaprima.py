5d3fcedf8424f47b26060e75e9add17b45e397f6
renatochagas.m@gmail.com <renatochagas.m@gmail.com>
2025-11-21 10:10:10 -0300

    esperto

--- src/core/backend/serializers/materiaprima.py
+++ src/core/backend/serializers/materiaprima.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from ..models.materiaPrima import MateriaPrima
 
+
 class MateriaPrimaSerializer(serializers.ModelSerializer):
     class Meta:
         model = MateriaPrima
-        fields = '__all__'
+        fields = "__all__"
