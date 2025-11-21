f4ac2ff8ea597d4e265be0db2972cc7cf65b3033
joaovnpt <joaovitorguiotti8@gmail.com>
2025-11-21 14:49:56 -0300

    FEAT: REgistrar no admin tabelas novas

--- src/core/backend/admin.py
+++ src/core/backend/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from core.backend.models import CategoriaAcervo, Colecao, Item, Localizacao, MateriaPrima, SubtipoMaterial
+from core.backend.models import CategoriaAcervo, Colecao, Item, Localizacao, MateriaPrima, SubtipoMaterial, Acervo, Colecionador
 
 admin.site.register(CategoriaAcervo)
 admin.site.register(Colecao)
@@ -7,3 +7,5 @@ admin.site.register(Item)
 admin.site.register(Localizacao)
 admin.site.register(MateriaPrima)
 admin.site.register(SubtipoMaterial)
+admin.site.register(Acervo)
+admin.site.register(Colecionador)
