e67ff4278fc83fcf8175a79f2dbb276c048f2353
joaovnpt <joaovitorguiotti8@gmail.com>
2025-11-21 14:28:03 -0300

    FEAT: Uploader configs

--- src/config/urls.py
+++ src/config/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from core.backend.views import LocalizacaoViewSet, MateriaPrimaViewSet, ItemAcervoViewSet, ColecaoViewSet
+from django.conf import settings
+from django.conf.urls.static import static
+from uploader.router import router as uploader_router
 
 
 from rest_framework_simplejwt.views import (
@@ -25,4 +28,7 @@ urlpatterns = [
     path('api/', include((router.urls, 'api'), namespace='api')),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/media/", include(uploader_router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
